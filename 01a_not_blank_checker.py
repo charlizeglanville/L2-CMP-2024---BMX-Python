a08dc49bd6c7e8d840cec9ccd8df07da85ac9732
charlizeglanville <24548@tgs.school.nz>
2024-07-31 11:30:04 +1200

    Add files via upload
    This file includes my first component which is the not blank statement and asking the rider for there name.

---
+++ 01a_not_blank_checker.py
@@ -0,0 +1,19 @@
+# checks that user response is not blank
+def not_blank(question):
+    while True:
+        response = input(question)
+        if response == "" or response == " ":
+            print("Sorry, this can't be blank. Please try again.")
+        else:
+            return response
+
+
+while True:
+    name = not_blank("Enter your name (or exit using 'xx'): ")
+    # Exit Loop if users type 'xx'
+    if name == 'xx':
+        continue
+    else:
+        print()
+        print('Successfully Closed')
+        break
